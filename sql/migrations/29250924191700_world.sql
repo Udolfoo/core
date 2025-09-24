378395c683c67d981c78ca0857c9532b0ac0c4d8
Udo <danny337@hotmail.de>
2025-09-24 19:24:29 +0200

    Fix Legion Hound Damage

--- sql/migrations/29250924191700_world.sql
+++ sql/migrations/29250924191700_world.sql
@@ -11,6 +11,9 @@ INSERT INTO `migrations` VALUES ('20250817105838');
 --Wrong Linking in WC by Druid of the Fang
 DELETE FROM creature_groups WHERE leader_guid=33964;
 
+-- Fix Legion Hound Damage in Ashenvale
+UPDATE `creature_template` SET `damage_multiplier` = '0.9' WHERE `creature_template`.`entry` = 6071 AND `creature_template`.`patch` = 0;
+
 -- End of migration.
 END IF;
 END??
