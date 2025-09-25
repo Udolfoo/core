cc07779d6451ed05dad0c0652e281fcc5d97b5e8
Udo <danny337@hotmail.de>
2025-09-25 12:01:18 +0200

    Wrong SQL Typo

--- sql/migrations/20250924191700_world.sql
+++ sql/migrations/20250924191700_world.sql
@@ -8,13 +8,14 @@ IF v = 0 THEN
 INSERT INTO `migrations` VALUES ('20250924191700');
 -- Add your query below.
 
---Wrong Linking in WC by Druid of the Fang
-DELETE FROM creature_groups WHERE leader_guid=33964;
+-- Wrong Linking in WC by Druid of the Fang
+DELETE FROM creature_groups WHERE `creature_groups`.`member_guid` = 33963;
+DELETE FROM creature_groups WHERE `creature_groups`.`member_guid` = 33962;
 
 -- Fix Legion Hound Damage in Ashenvale
 UPDATE `creature_template` SET `damage_multiplier` = '0.9' WHERE `creature_template`.`entry` = 6071 AND `creature_template`.`patch` = 0;
 
---Fix All Chests Respawns due to Economy
+-- Fix All Chests Respawns due to Economy
 UPDATE gameobject SET spawntimesecsmin = 7200, spawntimesecsmax = 7200 WHERE id IN ( 2850, 2852, 2855, 4149, 153451, 153453, 153454, 153463, 2843, 2844, 2849, 106318, 106319 );
 
 -- End of migration.
