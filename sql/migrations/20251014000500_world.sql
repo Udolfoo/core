f733a4d48cece1635f9663947494977d626ac941
Udo <danny337@hotmail.de>
2025-10-14 00:08:32 +0200

    Fix Skinable Mobs in Dustwallow Marsh

---
+++ sql/migrations/20251014000500_world.sql
@@ -0,0 +1,26 @@
+DROP PROCEDURE IF EXISTS add_migration;
+DELIMITER ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20251014000500');
+IF v = 0 THEN
+INSERT INTO `migrations` VALUES ('20251014000500');
+-- Add your query below.
+
+-- Fix Skinable Mobs in Dustwallow Marsh
+UPDATE `skinning_loot_template`
+SET `groupid` = 1
+WHERE `groupid` = 0
+  AND `entry` IN (
+    4356,4355,4388,4343,4398,4397,4396,4357,4352,4345,4342,4344,4351,4341,
+    4329,4334,4328,4323,4331,4324,
+    14232,14233,14234,4841,4339,10321
+  );
+
+-- End of migration.
+END IF;
+END??
+DELIMITER ;
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
